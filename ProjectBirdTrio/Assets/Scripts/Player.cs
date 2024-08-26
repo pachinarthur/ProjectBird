f3b38e85fe8124566d4266008cfb40fed51bbed0
Seiko34 <Seiko34000@gmail.com>
2024-08-26 16:14:08 +0200

    Player Test

--- ProjectBirdTrio/Assets/Scripts/Player.cs
+++ ProjectBirdTrio/Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Char_Bird : MonoBehaviour
+public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
