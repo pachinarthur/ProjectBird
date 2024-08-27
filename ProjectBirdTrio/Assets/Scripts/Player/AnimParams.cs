c57d1cf0cafba4e98f9b36faab13cb78e829ed6f
Seiko34 <Seiko34000@gmail.com>
2024-08-27 17:40:19 +0200

    Revert "Fly and Land"
    This reverts commit b19285d7a57cc2c97b9d598f9b4652b78cf0d0b2.

--- ProjectBirdTrio/Assets/Scripts/Player/AnimParams.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class AnimParams
-{
-    public const string FORWARD_PARAM = "forward";
-    public const string RIGHT_PARAM = "right";
-}
